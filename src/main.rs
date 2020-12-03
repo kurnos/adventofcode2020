d6e60426144b0e79afc8d3c667bd65a6a8910da9
Johan Olsson <kurnos@gmail.com>
2020-12-03 06:44:15 +0100

    Solve day3

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use infra::{run_day, FromFile};
 
 mod day1;
 mod day2;
+mod day3;
 mod infra;
 
 fn main() {
+    let day = Some(3u8);
     run_day(
-        None,
+        day,
         1,
         day1::Day1,
         (FromFile("day1.txt"), 956091),
@@ -14,10 +16,12 @@ fn main() {
     );
 
     run_day(
-        None,
+        day,
         1,
         day2::Day2,
         (FromFile("day2.txt"), 519),
         (FromFile("day2.txt"), 708)
     );
+
+    day3::main();
 }
