03adaa9472a51c2bd1c8105f7fc11770431ae9da
Johan Olsson <kurnos@gmail.com>
2020-12-10 09:32:44 +0100

    Make day2 faster

--- src/day2.rs
+++ src/day2.rs
@@ -1,5 +1,4 @@
 use crate::infra::Problem;
-use crate::parse;
 
 pub struct Day2;
 
@@ -11,8 +10,7 @@ impl Problem<String, String, usize, usize> for Day2 {
         contents
             .lines()
             .map(|line| {
-                let (min, max, letter, password): (usize, usize, char, String) =
-                    parse!(r"(.+)-(.+) (.): (.+)", line, 4);
+                let (min, max, letter, password) = parse(line);
                 let count = password.matches(letter).count();
                 min <= count && count <= max
             })
@@ -23,12 +21,22 @@ impl Problem<String, String, usize, usize> for Day2 {
         contents
             .lines()
             .map(|line| {
-                let (min, max, letter, password): (usize, usize, char, String) =
-                    parse!(r"(.+)-(.+) (.): (.+)", line, 4);
+                let (min, max, letter, password) = parse(line);
                 (password.chars().nth(min - 1) == Some(letter))
                     ^ (password.chars().nth(max - 1) == Some(letter))
             })
             .filter(|a| *a)
             .count()
     }
+}
+
+fn parse<'a>(line: &'a str) -> (usize, usize, char, &'a str) {
+    let mut x = line.splitn(2, "-");
+    let r1 = x.next().unwrap().parse().unwrap();
+    let mut x = x.next().unwrap().splitn(2, " ");
+    let r2 = x.next().unwrap().parse().unwrap();
+    let mut x = x.next().unwrap().splitn(2, ": ");
+    let r3 = x.next().unwrap().parse().unwrap();
+    let r4 = x.next().unwrap();
+    (r1, r2, r3, r4)
 }
