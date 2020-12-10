1c47f29d58cbdf165a31121321499478c1b5ba04
Johan Olsson <kurnos@gmail.com>
2020-12-10 10:14:22 +0100

    Two digit days

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use infra::{run_day, FromFile};
 
-mod day1;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
 mod day10;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
 mod infra;
 
 fn main() {
@@ -17,7 +17,7 @@ fn main() {
     run_day(
         day,
         1,
-        day1::Day1,
+        day01::Day1,
         (FromFile("day1.txt"), 956091),
         (FromFile("day1.txt"), 79734368),
     );
@@ -25,7 +25,7 @@ fn main() {
     run_day(
         day,
         1,
-        day2::Day2,
+        day02::Day2,
         (FromFile("day2.txt"), 519),
         (FromFile("day2.txt"), 708),
     );
@@ -33,7 +33,7 @@ fn main() {
     run_day(
         day,
         1,
-        day3::Day3,
+        day03::Day3,
         (FromFile("day3.txt"), 216),
         (FromFile("day3.txt"), 6708199680),
     );
@@ -41,7 +41,7 @@ fn main() {
     run_day(
         day,
         1,
-        day4::Day4,
+        day04::Day4,
         (FromFile("day4.txt"), 250),
         (FromFile("day4.txt"), 158),
     );
@@ -49,7 +49,7 @@ fn main() {
     run_day(
         day,
         1,
-        day5::Day5,
+        day05::Day5,
         (FromFile("day5.txt"), 832),
         (FromFile("day5.txt"), 517),
     );
@@ -57,7 +57,7 @@ fn main() {
     run_day(
         day,
         1,
-        day6::Day6,
+        day06::Day6,
         (FromFile("day6.txt"), 6782),
         (FromFile("day6.txt"), 3596),
     );
@@ -65,7 +65,7 @@ fn main() {
     run_day(
         day,
         1,
-        day7::Day7,
+        day07::Day7,
         (FromFile("day7.txt"), 128),
         (FromFile("day7.txt"), 20189),
     );
@@ -73,7 +73,7 @@ fn main() {
     run_day(
         day,
         1,
-        day8::Day8,
+        day08::Day8,
         (FromFile("day8.txt"), 1814),
         (FromFile("day8.txt"), 1056),
     );
@@ -81,7 +81,7 @@ fn main() {
     run_day(
         day,
         1,
-        day9::Day9,
+        day09::Day9,
         (FromFile("day9.txt"), 36845998),
         (FromFile("day9.txt"), 4830226),
     );
