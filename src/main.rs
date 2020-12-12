e186d42467e0524fe4d68384d07c0acd23400c71
Johan Olsson <kurnos@gmail.com>
2020-12-12 19:02:26 +0100

    Args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use infra::{run_day, FromFile};
+use std::env;
 
 mod day01;
 mod day02;
@@ -15,10 +16,15 @@ mod day12;
 mod infra;
 
 fn main() {
-    let day = Some(12);
+    let day = env::args().nth(1).and_then(|d| d.parse::<u8>().ok());
+    let times = env::args()
+        .nth(2)
+        .and_then(|d| d.parse::<u32>().ok())
+        .unwrap_or(1);
+
     run_day(
         day,
-        1,
+        times,
         day01::Day1,
         (FromFile("day1.txt"), 956091),
         (FromFile("day1.txt"), 79734368),
@@ -26,7 +32,7 @@ fn main() {
 
     run_day(
         day,
-        1,
+        times,
         day02::Day2,
         (FromFile("day2.txt"), 519),
         (FromFile("day2.txt"), 708),
@@ -34,7 +40,7 @@ fn main() {
 
     run_day(
         day,
-        1,
+        times,
         day03::Day3,
         (FromFile("day3.txt"), 216),
         (FromFile("day3.txt"), 6708199680),
@@ -42,7 +48,7 @@ fn main() {
 
     run_day(
         day,
-        1,
+        times,
         day04::Day4,
         (FromFile("day4.txt"), 250),
         (FromFile("day4.txt"), 158),
@@ -50,7 +56,7 @@ fn main() {
 
     run_day(
         day,
-        1,
+        times,
         day05::Day5,
         (FromFile("day5.txt"), 832),
         (FromFile("day5.txt"), 517),
@@ -58,7 +64,7 @@ fn main() {
 
     run_day(
         day,
-        1,
+        times,
         day06::Day6,
         (FromFile("day6.txt"), 6782),
         (FromFile("day6.txt"), 3596),
@@ -66,7 +72,7 @@ fn main() {
 
     run_day(
         day,
-        1,
+        times,
         day07::Day7,
         (FromFile("day7.txt"), 128),
         (FromFile("day7.txt"), 20189),
@@ -74,7 +80,7 @@ fn main() {
 
     run_day(
         day,
-        1,
+        times,
         day08::Day8,
         (FromFile("day8.txt"), 1814),
         (FromFile("day8.txt"), 1056),
@@ -82,7 +88,7 @@ fn main() {
 
     run_day(
         day,
-        1,
+        times,
         day09::Day9,
         (FromFile("day9.txt"), 36845998),
         (FromFile("day9.txt"), 4830226),
@@ -90,7 +96,7 @@ fn main() {
 
     run_day(
         day,
-        1,
+        times,
         day10::Day10,
         (FromFile("day10.txt"), 2263),
         (FromFile("day10.txt"), 396857386627072),
@@ -98,7 +104,7 @@ fn main() {
 
     run_day(
         day,
-        1,
+        times,
         day11::Day11,
         (FromFile("day11.txt"), 2361),
         (FromFile("day11.txt"), 2119),
@@ -106,7 +112,7 @@ fn main() {
 
     run_day(
         day,
-        1,
+        times,
         day12::Day12,
         (FromFile("day12.txt"), 381),
         (FromFile("day12.txt"), 28591),
