056baf4a630e8dab3c8fed78cf2a4ecb042a2f61
Johan Olsson <kurnos@gmail.com>
2020-12-15 21:07:57 +0100

    Make day15 faster

--- src/day15.rs
+++ src/day15.rs
@@ -1,5 +1,4 @@
 use crate::infra::Problem;
-use std::collections::HashMap;
 
 pub struct Day15;
 
@@ -17,25 +16,20 @@ impl Problem<Vec<u32>, Vec<u32>, u32, u32> for Day15 {
     }
 }
 
-fn run_with(preamble: &[u32], n: u32) -> u32 {
-    let mut last_spoken = HashMap::new();
+fn run_with(preamble: &[u32], limit: u32) -> u32 {
+    let mut last_spoken = vec![0; limit as usize];
 
-    for (t, n) in preamble.iter().take(preamble.len() - 1).enumerate() {
-        last_spoken.insert(*n, t as u32);
+    for (t, &n) in preamble.iter().take(preamble.len() - 1).enumerate() {
+        last_spoken[n as usize] = t as u32 + 1;
     }
 
     let mut current = preamble[preamble.len() - 1];
-    let mut turn = preamble.len() as u32 - 1;
 
-    while turn < n - 1 {
-        if let Some(n) = last_spoken.get(&current).cloned() {
-            last_spoken.insert(current, turn);
-            current = turn - n
-        } else {
-            last_spoken.insert(current, turn);
-            current = 0;
-        }
-        turn += 1;
+    for turn in preamble.len() as u32..limit {
+        let n = last_spoken[current as usize];
+        last_spoken[current as usize] = turn;
+        current = if n != 0 { turn - n } else { 0 };
     }
+
     current
 }
