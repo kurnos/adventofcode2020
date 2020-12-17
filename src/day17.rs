941fa5bfd5b809ad08e2fa1c587f7a6665d3e0b5
Johan Olsson <kurnos@gmail.com>
2020-12-17 14:32:30 +0100

    Make day17 faster

--- src/day17.rs
+++ src/day17.rs
@@ -1,7 +1,7 @@
 use crate::infra::Problem;
-use itertools::{iproduct, Itertools};
-use nalgebra::{Point3, Point4, Vector3, Vector4};
-use std::collections::HashSet;
+use itertools::iproduct;
+use nalgebra::{Point3, Point4};
+use std::collections::{HashMap, HashSet};
 
 pub struct Day17;
 
@@ -11,105 +11,78 @@ impl Problem<String, String, usize, usize> for Day17 {
     }
 
     fn first(contents: String) -> usize {
-        let mut data: HashSet<Point3<i16>> = contents
-            .lines()
-            .enumerate()
-            .flat_map(|(y, line)| {
-                line.bytes()
-                    .enumerate()
-                    .map(move |(x, line)| (Point3::new(x as i16, y as i16, 0), line))
-            })
-            .filter_map(|(p, b)| if b == b'#' { Some(p) } else { None })
+        let mut data: HashSet<Point3<i8>> = parse(&contents)
+            .map(|(x, y)| Point3::new(x as i8, y as i8, 0))
             .collect();
 
-        let mut new_data = data.clone();
-
+        let mut weights = HashMap::new();
         for _ in 0..6 {
-            let dx = data.iter().map(|p| p.x).minmax().into_option().unwrap();
-            let dy = data.iter().map(|p| p.y).minmax().into_option().unwrap();
-            let dz = data.iter().map(|p| p.z).minmax().into_option().unwrap();
-            for x in dx.0 - 1..=dx.1 + 1 {
-                for y in dy.0 - 1..=dy.1 + 1 {
-                    for z in dz.0 - 1..=dz.1 + 1 {
-                        let p = Point3::new(x, y, z);
-                        let a = data.contains(&p);
-                        let n_count = ncount3(&data, &p);
-                        if a && (n_count == 2 || n_count == 3) || !a && n_count == 3 {
-                            new_data.insert(p);
-                        } else {
-                            new_data.remove(&p);
-                        }
-                    }
+            for p in data.iter() {
+                for n in ns3(*p) {
+                    *weights.entry(*p).or_insert(0u8) |= 128;
+                    *weights.entry(n).or_insert(0u8) += 1;
                 }
             }
-            std::mem::swap(&mut data, &mut new_data);
-            new_data.clear();
+            for (p, m) in weights.iter() {
+                let a = (m >> 7) != 0;
+                let c = m & 127;
+                if a && !(c == 2 || c == 3) {
+                    data.remove(p);
+                } else if !a && c == 3 {
+                    data.insert(*p);
+                }
+            }
+            weights.clear();
         }
         data.len()
     }
 
     fn second(contents: String) -> usize {
-        let ns: Vec<Vector4<i32>> = iproduct!(-1..=1, -1..=1, -1..=1, -1..=1)
-            .filter(|&(x, y, z, w)| !(x == 0 && y == 0 && z == 0 && w == 0))
-            .map(|v| Vector4::new(v.0, v.1, v.2, v.3))
-            .collect_vec();
-        assert_eq!(ns.len(), 80);
-
-        let mut data: HashSet<Point4<i32>> = contents
-            .lines()
-            .enumerate()
-            .flat_map(|(y, line)| {
-                line.bytes()
-                    .enumerate()
-                    .map(move |(x, line)| (Point4::new(x as i32, y as i32, 0, 0), line))
-            })
-            .filter_map(|(p, b)| if b == b'#' { Some(p) } else { None })
+        let mut data: HashSet<Point4<i8>> = parse(&contents)
+            .map(|(x, y)| Point4::new(x as i8, y as i8, 0, 0))
             .collect();
 
-        let mut new_data = data.clone();
-
+        let mut weights = HashMap::new();
         for _ in 0..6 {
-            let dx = data.iter().map(|p| p.x).minmax().into_option().unwrap();
-            let dy = data.iter().map(|p| p.y).minmax().into_option().unwrap();
-            let dz = data.iter().map(|p| p.z).minmax().into_option().unwrap();
-            let dw = data.iter().map(|p| p.w).minmax().into_option().unwrap();
-            for x in dx.0 - 1..=dx.1 + 1 {
-                for y in dy.0 - 1..=dy.1 + 1 {
-                    for z in dz.0 - 1..=dz.1 + 1 {
-                        for w in dw.0 - 1..=dw.1 + 1 {
-                            let p = Point4::new(x, y, z, w);
-                            let a = data.contains(&p);
-                            let n_count = ncount4(&data, &p);
-                            if a && (n_count == 2 || n_count == 3) || !a && n_count == 3 {
-                                new_data.insert(p);
-                            } else {
-                                new_data.remove(&p);
-                            }
-                        }
-                    }
+            for p in data.iter() {
+                for n in ns4(*p) {
+                    *weights.entry(*p).or_insert(0u8) |= 128;
+                    *weights.entry(n).or_insert(0u8) += 1;
                 }
             }
-            std::mem::swap(&mut data, &mut new_data);
-            new_data.clear();
+            for (p, m) in weights.iter() {
+                let a = (m >> 7) != 0;
+                let c = m & 127;
+                if a && !(c == 2 || c == 3) {
+                    data.remove(p);
+                } else if !a && c == 3 {
+                    data.insert(*p);
+                }
+            }
+            weights.clear();
         }
         data.len()
     }
 }
 
-fn ncount3(board: &HashSet<Point3<i16>>, p: &Point3<i16>) -> usize {
-    let x = &[0i16, 1, -1];
-    iproduct!(x, x, x)
-        .skip(1)
-        .map(|(&x, &y, &z)| Vector3::new(x, y, z))
-        .filter(|d| board.contains(&(*p + *d)))
-        .count()
+fn parse<'a>(contents: &'a str) -> impl Iterator<Item = (usize, usize)> + 'a {
+    contents
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| line.bytes().enumerate().map(move |(x, b)| ((x, y), b)))
+        .filter_map(|(p, b)| if b == b'#' { Some(p) } else { None })
 }
 
-fn ncount4(board: &HashSet<Point4<i32>>, p: &Point4<i32>) -> usize {
+fn ns3(p: Point3<i8>) -> impl Iterator<Item = Point3<i8>> {
     let x = &[0, 1, -1];
+    iproduct!(x, x, x)
+        .skip(1)
+        .map(move |(&x, &y, &z)| Point3::new(p.x + x, p.y + y, p.z + z))
+}
+
+fn ns4(p: Point4<i8>) -> impl Iterator<Item = Point4<i8>> {
+    let x = &[0i8, 1, -1];
     iproduct!(x, x, x, x)
         .skip(1)
-        .map(|(&x, &y, &z, &w)| Vector4::new(x, y, z, w))
-        .filter(|d| board.contains(&(*p + *d)))
-        .count()
+        .map(move |(&x, &y, &z, &w)| Point4::new(p.x + x, p.y + y, p.z + z, p.w + w))
 }
