79743df0fd35dd8994a0ee42be88d95be2768ed7
Johan Olsson <kurnos@gmail.com>
2020-12-24 23:04:18 +0100

    Make day24 faster

--- src/day24.rs
+++ src/day24.rs
@@ -1,8 +1,5 @@
-use std::collections::{HashMap, HashSet};
-
-use itertools::Itertools;
-
 use crate::infra::Problem;
+use std::collections::{HashMap, HashSet};
 
 pub struct Day24;
 
@@ -12,144 +9,64 @@ impl Problem<String, String, usize, usize> for Day24 {
     }
 
     fn first(contents: String) -> usize {
-        let data = contents
-            .lines()
-            .map(|x| parse_line(x.as_bytes()))
-            .collect_vec();
-
-        let mut grid = HashMap::new();
-        for pattern in data {
-            let mut pos = (0, 0, 0);
-            for n in pattern {
-                pos = match n {
-                    N::E => (pos.0 + 1, pos.1, pos.2 - 1),
-                    N::SE => (pos.0, pos.1 + 1, pos.2 - 1),
-                    N::SW => (pos.0 - 1, pos.1 + 1, pos.2),
-                    N::W => (pos.0 - 1, pos.1, pos.2 + 1),
-                    N::NW => (pos.0, pos.1 - 1, pos.2 + 1),
-                    N::NE => (pos.0 + 1, pos.1 - 1, pos.2),
-                }
-            }
-            *grid.entry(pos).or_insert(false) ^= true;
-        }
-
-        let board = grid
-            .into_iter()
-            .filter_map(|(p, b)| if b { Some(p) } else { None })
-            .collect::<HashSet<_>>();
-        board.len()
+        make_board(contents).len()
     }
 
     fn second(contents: String) -> usize {
-        let data = contents
-            .lines()
-            .map(|x| parse_line(x.as_bytes()))
-            .collect_vec();
-
-        let mut grid = HashMap::new();
-        for pattern in data {
-            let mut pos = (0i8, 0i8, 0i8);
-            for n in pattern {
-                pos = match n {
-                    N::E => (pos.0 + 1, pos.1, pos.2 - 1),
-                    N::SE => (pos.0, pos.1 + 1, pos.2 - 1),
-                    N::SW => (pos.0 - 1, pos.1 + 1, pos.2),
-                    N::W => (pos.0 - 1, pos.1, pos.2 + 1),
-                    N::NW => (pos.0, pos.1 - 1, pos.2 + 1),
-                    N::NE => (pos.0 + 1, pos.1 - 1, pos.2),
-                }
-            }
-            *grid.entry(pos).or_insert(false) ^= true;
-        }
-
-        let mut board = grid
-            .into_iter()
-            .filter_map(|(p, b)| if b { Some(p) } else { None })
-            .collect::<HashSet<_>>();
-
+        let mut board = make_board(contents);
+        let mut counts = HashMap::new();
         for _ in 0..100 {
-            board = evolve(&board);
+            for &(x, y) in board.iter() {
+                *counts.entry((x, y)).or_insert(0u8) |= 8;
+                *counts.entry((x + 1, y)).or_insert(0) += 1;
+                *counts.entry((x + 1, y - 1)).or_insert(0) += 1;
+                *counts.entry((x, y + 1)).or_insert(0) += 1;
+                *counts.entry((x, y - 1)).or_insert(0) += 1;
+                *counts.entry((x - 1, y + 1)).or_insert(0) += 1;
+                *counts.entry((x - 1, y)).or_insert(0) += 1;
+            }
+
+            counts.iter().for_each(|(p, c)| {
+                let is_black = (c & 8) != 0;
+                let count = c & 7;
+                if is_black && (count == 0 || count > 2) {
+                    board.remove(p);
+                } else if *c == 2 {
+                    board.insert(*p);
+                }
+            });
+            counts.clear();
         }
         board.len()
     }
 }
 
-fn evolve(board: &HashSet<(i8, i8, i8)>) -> HashSet<(i8, i8, i8)> {
-    let mut counts = HashMap::new();
-    for &(x, y, z) in board {
-        *counts.entry((x, y, z)).or_insert(0u8) |= 8;
-        for &(dx, dy, dz) in [
-            (1, 0, -1),
-            (1, -1, 0),
-            (0, 1, -1),
-            (0, -1, 1),
-            (-1, 1, 0),
-            (-1, 0, 1),
-        ]
-        .iter()
-        {
-            *counts.entry((x + dx, y + dy, z + dz)).or_insert(0) += 1;
-        }
-    }
-
-    counts
-        .into_iter()
-        .filter_map(|(p, c)| {
-            let is_black = (c & 8) != 0;
-            let count = c & 7;
-            if is_black && (count == 0 || count > 2) {
-                None
-            } else if !is_black && (count == 2) {
-                Some(p)
-            } else if is_black {
-                Some(p)
-            } else {
-                None
-            }
-        })
-        .collect()
-}
-
-#[derive(Debug)]
-enum N {
-    E,
-    SE,
-    SW,
-    W,
-    NW,
-    NE,
-}
-
-fn parse_line(mut line: &[u8]) -> Vec<N> {
-    let mut res = vec![];
-    while !line.is_empty() {
-        line = match line {
-            [b'e', rest @ ..] => {
-                res.push(N::E);
-                rest
-            }
-            [b's', b'e', rest @ ..] => {
-                res.push(N::SE);
-                rest
-            }
-            [b's', b'w', rest @ ..] => {
-                res.push(N::SW);
-                rest
-            }
-            [b'n', b'w', rest @ ..] => {
-                res.push(N::NW);
-                rest
-            }
-            [b'n', b'e', rest @ ..] => {
-                res.push(N::NE);
-                rest
-            }
-            [b'w', rest @ ..] => {
-                res.push(N::W);
-                rest
-            }
-            s => panic!(String::from_utf8(s.to_vec())),
+fn make_board(contents: String) -> HashSet<(i8, i8)> {
+    let mut res = HashSet::new();
+    for pos in contents.lines().map(parse_line) {
+        if res.contains(&pos) {
+            res.remove(&pos);
+        } else {
+            res.insert(pos);
         }
     }
     res
+}
+
+fn parse_line(line: &str) -> (i8, i8) {
+    let mut pos = (0, 0);
+    let mut prev = 0;
+    for b in line.bytes() {
+        match (prev, b) {
+            (b's', b'e') => pos.1 += 1,
+            (b's', b'w') => pos = (pos.0 - 1, pos.1 + 1),
+            (b'n', b'e') => pos = (pos.0 + 1, pos.1 - 1),
+            (b'n', b'w') => pos.1 -= 1,
+            (_, b'e') => pos.0 += 1,
+            (_, b'w') => pos.0 -= 1,
+            _ => {}
+        }
+        prev = b;
+    }
+    pos
 }
